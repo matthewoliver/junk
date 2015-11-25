2eb70e9425d0d54162b9675f84447f0b239276ed
Matthew Oliver <matt@oliver.net.au>
2015-11-25 11:39:58 +1100

    Updated saio.sh

--- swift/saio.sh
+++ swift/saio.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-
+DEBIAN=0
+REDHAT=1
+SIZE=1
 UNIT_TESTS=1
 
-sudo apt-get update
-sudo apt-get install -y curl gcc memcached rsync sqlite3 xfsprogs \
-                     git-core libffi-dev python-setuptools
-sudo apt-get install -y python-coverage python-dev python-nose \
-                     python-simplejson python-xattr python-eventlet \
-                     python-greenlet python-pastedeploy \
-                     python-netifaces python-pip python-dnspython \
-                     python-mock
+DISTRO=$DEBIAN
+
+if [ $DISTRO -eq $DEBIAN ]
+then
+  sudo apt-get update
+  sudo apt-get install -y curl gcc memcached rsync sqlite3 xfsprogs \
+                      git-core libffi-dev python-setuptools
+  sudo apt-get install -y python-coverage python-dev python-nose \
+                       python-simplejson python-xattr python-eventlet \
+                       python-greenlet python-pastedeploy \
+                       python-netifaces python-pip python-dnspython \
+                       python-mock
+else
+  sudo yum update
+  sudo yum install curl gcc memcached rsync sqlite xfsprogs git-core \
+                  libffi-devel xinetd python-setuptools \
+                  python-coverage python-devel python-nose \
+                  pyxattr python-eventlet \
+                  python-greenlet python-paste-deploy \
+                  python-netifaces python-pip python-dns \
+                  python-mock
+fi
 
 # Using loopback device
 sudo mkdir /srv
-sudo truncate -s 1GB /srv/swift-disk
+sudo truncate -s $(echo $SIZE)GB /srv/swift-disk
 sudo mkfs.xfs /srv/swift-disk
 
 # Add to fstab
@@ -30,7 +46,11 @@ sudo mount /mnt/sdb1
 sudo mkdir -p /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4
 sudo chown ${USER}:${USER} /mnt/sdb1/*
 for x in {1..4}; do sudo ln -s /mnt/sdb1/$x /srv/$x; done
-sudo mkdir -p /srv/1/node/sdb1 /srv/2/node/sdb2 /srv/3/node/sdb3 /srv/4/node/sdb4 /var/run/swift
+sudo mkdir -p /srv/1/node/sdb1 /srv/1/node/sdb5 \
+              /srv/2/node/sdb2 /srv/2/node/sdb6 \
+              /srv/3/node/sdb3 /srv/3/node/sdb7 \
+              /srv/4/node/sdb4 /srv/4/node/sdb8 \
+              /var/run/swift
 sudo chown -R ${USER}:${USER} /var/run/swift
 # **Make sure to include the trailing slash after /srv/$x/**
 for x in {1..4}; do sudo chown -R ${USER}:${USER} /srv/$x/; done
@@ -47,15 +67,29 @@ cd $HOME; git clone https://github.com/openstack/python-swiftclient.git
 cd $HOME/python-swiftclient; sudo python setup.py develop; cd -
 
 git clone https://github.com/openstack/swift.git
-cd $HOME/swift; sudo python setup.py develop; cd -
+cd $HOME/swift; sudo pip install -r requirements.txt; sudo python setup.py develop; cd -
+
+if [ $DISTRO -eq $REDHAT ]
+then
+  sudo pip install -U xattr
+fi
 
 sudo pip install -r swift/test-requirements.txt
 
 # setup rsync
 sudo cp $HOME/swift/doc/saio/rsyncd.conf /etc/
 sudo sed -i "s/<your-user-name>/${USER}/" /etc/rsyncd.conf
-sudo sed -i 's/^RSYNC_ENABLE=.*$/RSYNC_ENABLE=true/' /etc/default/rsync
-sudo service rsync restart
+if [ $DISTRO -eq $DEBIAN ]
+then
+  sudo sed -i 's/^RSYNC_ENABLE=.*$/RSYNC_ENABLE=true/' /etc/default/rsync
+  sudo service rsync restart
+else
+  sudo sed -i 's/^disable =.*$/disable = no/' /etc/xinetd.d/rsync
+  sudo setenforce Permissive
+  sudo systemctl restart xinetd.service
+  sudo systemctl enable rsyncd.service
+  sudo systemctl start rsyncd.service
+fi
 
 # Memcache
 sudo service memcached start
