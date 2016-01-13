0b206df29bc9ac3f3f50571a1d296ba0dfc68568
Matthew Oliver <matt@oliver.net.au>
2016-01-13 06:23:26 +0000

    Added some moew helper scripts I use for debugging swift code on a SAIO.

---
+++ swift/get_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+count=$(find /mnt/sdb1/ -name "*.db" |grep container | wc -l)
+i=0
+for cont in $(find /mnt/sdb1/ -name "*.db" |grep container)
+do
+  let "i+=1"
+  echo "Path: $cont"
+  swift-container-info $cont |grep -A 3 'Path:'
+  swift-container-info $cont |grep -A 11 '^Metadata:'
+  swift-container-info $cont |grep 'System Metadata:'
+  echo "Pivot_points"
+  sqlite3 $cont 'select * from pivot_points;'
+  echo "DB $i/$count"; echo
+  read
+done
---
+++ swift/show_root_pivot_tables.sh
@@ -0,0 +1,3 @@
+sqlite3 /mnt/sdb1/1/node/sdb1/containers/519/f49/81f282d1692b9634b67949db0dc56f49/81f282d1692b9634b67949db0dc56f49.db 'select * from pivot_points;'
+sqlite3 /mnt/sdb1/2/node/sdb2/containers/519/f49/81f282d1692b9634b67949db0dc56f49/81f282d1692b9634b67949db0dc56f49.db 'select * from pivot_points;'
+sqlite3 /mnt/sdb1/3/node/sdb3/containers/519/f49/81f282d1692b9634b67949db0dc56f49/81f282d1692b9634b67949db0dc56f49.db 'select * from pivot_points;'
