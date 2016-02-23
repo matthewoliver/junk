1026df517d0263bbe566c2cb70126e3bb2f59898
Matthew Oliver <matt@oliver.net.au>
2016-02-23 15:16:25 +1100

    Added git branch to PS1

--- swift/setup_saio_step2.sh
+++ swift/setup_saio_step2.sh
@@ -1,1 +1,14 @@
 git clone http://github.com/matthewoliver/junk; git clone https://github.com/matthewoliver/vim_config; cd vim_config; ./setup.sh; cd -; cd junk/swift;sudo apt-get install liberasurecode0 liberasurecode1 liberasurecode-dev; bash saio.sh
+
+
+cat >> ~/.bashrc <<EOF
+function git_branch() {
+  git branch &> /dev/null
+  if [ $? -eq 0 ]
+  then
+    echo "($(git branch |grep -e ^* |awk '{print $2}'))"
+  fi
+}
+export PS1="\u@\h:\w\$(git_branch)\$ "
+EOF
+
