a6efe3c0c4572a11e12a24fe39591652e29ef307
Matthew Oliver <matt@oliver.net.au>
2016-02-25 01:28:19 +0000

    Fixed up saio step2 a little

--- swift/setup_saio_step2.sh
+++ swift/setup_saio_step2.sh
@@ -4,9 +4,9 @@ git clone http://github.com/matthewoliver/junk; git clone https://github.com/mat
 cat >> ~/.bashrc <<EOF
 function git_branch() {
   git branch &> /dev/null
-  if [ $? -eq 0 ]
+  if [ \$? -eq 0 ]
   then
-    echo "($(git branch |grep -e ^* |awk '{print $2}'))"
+    echo "(\$(git branch |grep -e ^* |awk '{print \$2}'))"
   fi
 }
 export PS1="\u@\h:\w\$(git_branch)\$ "
