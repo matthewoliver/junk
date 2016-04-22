d2bc98d3de11dc468fb340eb771fd048109517ad
Matthew Oliver <matt@oliver.net.au>
2016-04-22 04:09:32 +0000

    Add size (df -h) to get_info.sh

--- swift/get_info.sh
+++ swift/get_info.sh
@@ -5,10 +5,11 @@ for cont in $(find /mnt/sdb1/ -name "*.db" |grep container)
 do
   let "i+=1"
   echo "Path: $cont"
+  echo "size: $(du -sh $cont |awk '{print $1}')"
   swift-container-info $cont |grep -A 3 'Path:'
   swift-container-info $cont |grep -A 11 '^Metadata:'
   swift-container-info $cont |grep 'System Metadata:'
-  echo "Pivot_points"
+  echo "Pivot_ranges"
   sqlite3 $cont 'select * from pivot_ranges;'
   echo "DB $i/$count"; echo
   read
