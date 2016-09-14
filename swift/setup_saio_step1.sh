1ff9f8fdf31bc446e5001c5e528688de8ca6be11
Matthew Oliver <matt@oliver.net.au>
2016-09-14 11:15:53 +1000

    Updates to the saio steps

--- swift/setup_saio_step1.sh
+++ swift/setup_saio_step1.sh
@@ -1,2 +1,2 @@
 # This should be done as root.
-apt-get update; apt-get install -y git htop screen vim-nox unzip fontconfig; adduser matt; gpasswd -a matt sudo; sudo su - matt
+apt-get update; apt-get install -y git htop tmux vim-nox unzip fontconfig; adduser matt; gpasswd -a matt sudo; sudo su - matt
