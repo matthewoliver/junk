fd48600efa32a9d34cb7e47b3dbc9b83fb40d741
Matthew Oliver <matt@oliver.net.au>
2016-09-14 01:44:38 +0000

    Added yum version

--- swift/setup_saio_step1.sh
+++ swift/setup_saio_step1.sh
@@ -1,2 +1,4 @@
 # This should be done as root.
 apt-get update; apt-get install -y git htop tmux vim-nox unzip fontconfig; adduser matt; gpasswd -a matt sudo; sudo su - matt
+
+# yum install git htop tmux vim unzip fontconfig; adduser matt; gpasswd -a matt wheel; passwd matt; sudo su - matt
