1f13db083a4ccbb271ebbdca17236c63716470a9
Matthew Oliver <matt@oliver.net.au>
2017-10-18 15:34:02 +1100

    Enhanced the gen_range_header script a little

--- gen_range_header.py
+++ gen_range_header.py
@@ -1,13 +1,32 @@
 import sys
 
 DEFAULT_NUM_RANGE = 20
+DEFAULT_MULTIPLIER = 1000
+DEFAULT_RANGE_SIZE = 10
+
 num = DEFAULT_NUM_RANGE
-if len(sys.argv) > 0:
+mult = DEFAULT_MULTIPLIER
+size = DEFAULT_RANGE_SIZE
+
+if len(sys.argv) > 1:
     try:
         num = int(sys.argv[1])
     except ValueError:
         num = DEFAULT_NUM_RANGE
 
-val = ("bytes=" + ",".join("%d-%d" % (x * 1000, x * 1000 + 10)
+if len(sys.argv) > 2:
+    try:
+        mult = int(sys.argv[2])
+    except ValueError:
+        mult = DEFAULT_MULTIPLIER
+
+if len(sys.argv) > 3:
+    try:
+        size = int(sys.argv[3])
+    except ValueError:
+        size = DEFAULT_RANGE_SIZE
+
+
+val = ("bytes=" + ",".join("%d-%d" % (x * mult, x * mult + size)
                            for x in range(num)))
 print("Ranges: %s" % val)
